37ffc5f31f0cbf4bf0f03689e4a60d5d33bfc6ca
adhammamdouh <36306083+adhammamdouh@users.noreply.github.com>
2019-10-22 15:33:33 +0200

    Update main.java

--- src/main.java
+++ src/main.java
@@ -1,18 +1,21 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 public class main {
 
-	public static void main(String[] args) throws IOException {
-		parser p = new parser();
-		boolean state = p.parse("exit");
-		if(state) {
-			String com = p.getCmd();
-			String[] arg= p.getArguments();
-//			System.out.println(com);
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+        parser p = new parser();
+        terminal term = new terminal();
+        while (true) {
+            System.out.print("Enter the Command : ");
+            String command = in.nextLine();
+            boolean state = p.parse(command);
+            if (state) {
+                term.excute(p.getCmd(), p.getArguments());
+            }
+        }
 
-			terminal b = new terminal(com,arg);
-		}
-
-	}
+    }
 
 }
